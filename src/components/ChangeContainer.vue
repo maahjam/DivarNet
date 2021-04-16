cf60b84c040f40984c9c85049bd52f73d1fb9909
mahsa jamshidian <maahjam@gmail.com>
2021-04-17 01:08:56 +0430

    Code formatting

--- src/components/ChangeContainer.vue
+++ src/components/ChangeContainer.vue
@@ -1,145 +1,160 @@
 <template>
-    <div>
-      <Filters @onFilterApplied="onFilterApplied"/>
-        <div class="__table">
-            <table id="change-table">
-              <thead>
-                <tr>
-                <th class="star"></th>
-                <th>نام تغییر دهنده</th>
-                <th class="date" @click="onDateClicked">{{getDateLableString()}}</th>
-                <th>نام آگهی</th>
-                <th>فیلد</th>
-                <th>مقدار قدیمی</th>
-                <th>مقدار جدید</th>
-                </tr>
-              </thead>
+  <div>
+    <Filters @onFilterApplied="onFilterApplied" />
+    <div class="__table">
+      <table id="change-table">
+        <thead>
+          <tr>
+            <th class="star"></th>
+            <th>نام تغییر دهنده</th>
+            <th class="date" @click="onDateClicked">
+              {{ getDateLableString() }}
+            </th>
+            <th>نام آگهی</th>
+            <th>فیلد</th>
+            <th>مقدار قدیمی</th>
+            <th>مقدار جدید</th>
+          </tr>
+        </thead>
 
-              <tbody>
-                <ChangeItem v-for="(change, i) in changes" :key="i" :change="change" :cellsCount="cellsCount" :isSelected="selectedChangesId.includes(change.id)" @onMarked="onChangeClicked"/>
-              </tbody>
-
-            </table>
-        </div>
-        <Pagination :totalPages="totalPages" @onCurrentPageChanged="onCurrentPageChanged"/>
+        <tbody>
+          <ChangeItem
+            v-for="(change, i) in changes"
+            :key="i"
+            :change="change"
+            :cellsCount="cellsCount"
+            :isSelected="selectedChangesId.includes(change.id)"
+            @onMarked="onChangeClicked"
+          />
+        </tbody>
+      </table>
     </div>
+    <Pagination
+      :totalPages="totalPages"
+      @onCurrentPageChanged="onCurrentPageChanged"
+    />
+  </div>
 </template>
 
 <script>
-import Filters from './Filters'
-import ChangeItem from './ChangeItem'
-import Pagination from './Pagination'
-import DataManager from '../utilities/DataManager'
-import LocalStorageManager from '../utilities/LocalStorageManager'
+import Filters from "./Filters";
+import ChangeItem from "./ChangeItem";
+import Pagination from "./Pagination";
+import DataManager from "../utilities/DataManager";
+import LocalStorageManager from "../utilities/LocalStorageManager";
 
 export default {
-    name: 'ChangeContainer',
-    data(){
-        return{
-            changes: [],
-            cellsCount: 0,
-            selectedChangesId: [],
-            perPage: 10,
-            currentPage: 1,
-            totalPages: 1,
-            ascSortByDate: null,
-            name: null,
-            date: null,
-            title: null,
-            field: null
-        }
+  name: "ChangeContainer",
+  data() {
+    return {
+      changes: [],
+      cellsCount: 0,
+      selectedChangesId: [],
+      perPage: 10,
+      currentPage: 1,
+      totalPages: 1,
+      ascSortByDate: null,
+      name: null,
+      date: null,
+      title: null,
+      field: null,
+    };
+  },
+  components: {
+    Filters,
+    ChangeItem,
+    Pagination,
+  },
+  methods: {
+    onChangeClicked(changeId) {
+      if (this.selectedChangesId.includes(changeId)) {
+        LocalStorageManager.removeSelectedChangeId(changeId);
+      } else {
+        LocalStorageManager.addSelectedChangeId(changeId);
+      }
+      this.selectedChangesId = LocalStorageManager.getSelectedChangesId();
     },
-    components:{
-      Filters,
-      ChangeItem,
-      Pagination
+    onCurrentPageChanged(newCurrentPage) {
+      this.currentPage = newCurrentPage;
+      this.changes = DataManager.getData(this.currentPage, this.perPage);
+      this.pushQuery();
     },
-    methods:{
-     onChangeClicked(changeId){
-        if (this.selectedChangesId.includes(changeId)){
-          LocalStorageManager.removeSelectedChangeId(changeId)
-        }else{
-          LocalStorageManager.addSelectedChangeId(changeId)
-        }
-        this.selectedChangesId = LocalStorageManager.getSelectedChangesId()
-      },
-      onCurrentPageChanged(newCurrentPage){
-        this.currentPage = newCurrentPage
-        this.changes = DataManager.getData(this.currentPage, this.perPage)
-        this.pushQuery()
-      },
-      onFilterApplied(name, date, title, field){
-        this.name = name
-        this.date = date
-        this.title = title
-        this.field = field
-        this.currentPage = 1
-        DataManager.filterData(this.name, this.date, this.title, this.field)
-        this.changes = DataManager.getData(this.currentPage, this.perPage)
-        this.totalPages = Math.ceil(DataManager.getTotalDataCount() / this.perPage)
-        this.pushQuery()
-      },
-      onDateClicked(){
-        if (this.ascSortByDate == null) {
-          this.ascSortByDate = true
-        }else{
-          this.ascSortByDate = !this.ascSortByDate
-        }
-        DataManager.sortDataByDate(this.ascSortByDate)
-        this.changes = DataManager.getData(this.currentPage, this.perPage)
-        this.pushQuery()
-      },
-      pushQuery(name, date, title, field){
-        let query = {}
-        if (this.currentPage !== 1){
-           query.page = this.currentPage
-        }
-        if (this.ascSortByDate !== null){
-           query.ascSortByDate = this.ascSortByDate
-        }
-        if (this.name){
-           query.name = this.name
-        }
-        if (this.date){
-           query.date = this.date
-        }
-        if (this.title){
-           query.title = this.title
-        }
-        if (this.field){
-           query.field = this.field
-        }
-        this.$router.push({path: '/', query: query })
-      },
-      getDateLableString(){
-        if (this.ascSortByDate == null){
-          return "تاریخ"
-        }else if (this.ascSortByDate){
-          return "تاریخ" + "   " + '▲'
-        }else{
-          return "تاریخ" + "   " + '▼'
-        }
+    onFilterApplied(name, date, title, field) {
+      this.name = name;
+      this.date = date;
+      this.title = title;
+      this.field = field;
+      this.currentPage = 1;
+      DataManager.filterData(this.name, this.date, this.title, this.field);
+      this.changes = DataManager.getData(this.currentPage, this.perPage);
+      this.totalPages = Math.ceil(
+        DataManager.getTotalDataCount() / this.perPage
+      );
+      this.pushQuery();
+    },
+    onDateClicked() {
+      if (this.ascSortByDate == null) {
+        this.ascSortByDate = true;
+      } else {
+        this.ascSortByDate = !this.ascSortByDate;
+      }
+      DataManager.sortDataByDate(this.ascSortByDate);
+      this.changes = DataManager.getData(this.currentPage, this.perPage);
+      this.pushQuery();
+    },
+    pushQuery(name, date, title, field) {
+      let query = {};
+      if (this.currentPage !== 1) {
+        query.page = this.currentPage;
+      }
+      if (this.ascSortByDate !== null) {
+        query.ascSortByDate = this.ascSortByDate;
+      }
+      if (this.name) {
+        query.name = this.name;
+      }
+      if (this.date) {
+        query.date = this.date;
+      }
+      if (this.title) {
+        query.title = this.title;
+      }
+      if (this.field) {
+        query.field = this.field;
+      }
+      this.$router.push({ path: "/", query: query });
+    },
+    getDateLableString() {
+      if (this.ascSortByDate == null) {
+        return "تاریخ";
+      } else if (this.ascSortByDate) {
+        return "تاریخ" + "   " + "▲";
+      } else {
+        return "تاریخ" + "   " + "▼";
       }
     },
-    mounted(){
-      if (this.$route.query.page){
-        this.currentPage = parseInt(this.$route.query.page)
-      }
-      if (this.$route.query.ascSortByDate){
-        this.ascSortByDate = this.$route.query.ascSortByDate == 'true'
-        DataManager.sortDataByDate(this.ascSortByDate)
-      }
-      this.name = this.$route.query.name
-      this.date = this.$route.query.date
-      this.title = this.$route.query.title
-      this.field = this.$route.query.field
-      DataManager.filterData(this.name, this.date, this.title, this.field)
-      this.totalPages = Math.ceil(DataManager.getTotalDataCount() / this.perPage)
-      this.changes = DataManager.getData(this.currentPage, this.perPage)
-      this.selectedChangesId = LocalStorageManager.getSelectedChangesId()
-      this.cellsCount = document.querySelector('#change-table').rows[0].cells.length
+  },
+  mounted() {
+    if (this.$route.query.page) {
+      this.currentPage = parseInt(this.$route.query.page);
     }
-}
+    if (this.$route.query.ascSortByDate) {
+      this.ascSortByDate = this.$route.query.ascSortByDate == "true";
+      DataManager.sortDataByDate(this.ascSortByDate);
+    }
+    this.name = this.$route.query.name;
+    this.date = this.$route.query.date;
+    this.title = this.$route.query.title;
+    this.field = this.$route.query.field;
+    DataManager.filterData(this.name, this.date, this.title, this.field);
+    this.totalPages = Math.ceil(DataManager.getTotalDataCount() / this.perPage);
+    this.changes = DataManager.getData(this.currentPage, this.perPage);
+    this.selectedChangesId = LocalStorageManager.getSelectedChangesId();
+    this.cellsCount = document.querySelector(
+      "#change-table"
+    ).rows[0].cells.length;
+  },
+};
 </script>
 
 <style scoped>
@@ -159,32 +174,30 @@ th {
   border: 2px solid gray;
 }
 
-.__table{
-    overflow-x: auto;
-    direction: rtl;
+.__table {
+  overflow-x: auto;
+  direction: rtl;
 }
 
-.star{
-    min-width: 25px;
-    width: 25px;
-    max-width: 25px;
+.star {
+  min-width: 25px;
+  width: 25px;
+  max-width: 25px;
 }
 
-.date{
-   cursor: pointer;
-  -webkit-touch-callout: none; /* iOS Safari */
-    -webkit-user-select: none; /* Safari */
-     -khtml-user-select: none; /* Konqueror HTML */
-       -moz-user-select: none; /* Old versions of Firefox */
-        -ms-user-select: none; /* Internet Explorer/Edge */
-            user-select: none; /* Non-prefixed version, currently
-                                  supported by Chrome, Edge, Opera and Firefox */
-
+.date {
+  cursor: pointer;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none; 
+  -khtml-user-select: none; 
+  -moz-user-select: none; 
+  -ms-user-select: none; 
+  user-select: none;
 }
 
-tr{
-    min-height: 50px;
-    height: 50px;
-    max-height: 50px;
+tr {
+  min-height: 50px;
+  height: 50px;
+  max-height: 50px;
 }
 </style>
--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,102 +1,108 @@
 <template>
-    <div class="container">
-        <div class="buttons" @click="onNextClicked">بعدی</div>
-        <div class="result">{{"از " + totalPages}}</div>
-        <input id="input" type="number" v-model.number="currentPage" min="1" :max="totalPages">
-        <div class="buttons" @click="onPreviousClicked">قبلی</div>
-    </div>
+  <div class="container">
+    <div class="buttons" @click="onNextClicked">بعدی</div>
+    <div class="result">{{ "از " + totalPages }}</div>
+    <input
+      id="input"
+      type="number"
+      v-model.number="currentPage"
+      min="1"
+      :max="totalPages"
+    />
+    <div class="buttons" @click="onPreviousClicked">قبلی</div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Pagination',
-    data(){
-        return{
-            currentPage: 1,
-        }
-    },
-    watch: {
-      currentPage: function (newVal, oldVal) {
-          let newCurrentPage = parseInt(newVal)
-          if (Number.isNaN(newCurrentPage)) {return}
-          if (newCurrentPage > this.totalPages){
-              this.currentPage = this.totalPages
-          }else if (newCurrentPage < 1){
-              this.currentPage = 1
-          }else{
-              this.$emit('onCurrentPageChanged', this.currentPage)
-          }
+  name: "Pagination",
+  data() {
+    return {
+      currentPage: 1,
+    };
+  },
+  watch: {
+    currentPage: function (newVal, oldVal) {
+      let newCurrentPage = parseInt(newVal);
+      if (Number.isNaN(newCurrentPage)) {
+        return;
+      }
+      if (newCurrentPage > this.totalPages) {
+        this.currentPage = this.totalPages;
+      } else if (newCurrentPage < 1) {
+        this.currentPage = 1;
+      } else {
+        this.$emit("onCurrentPageChanged", this.currentPage);
+      }
     },
   },
-   props: {
+  props: {
     totalPages: Number,
   },
-   mounted(){
-      if (this.$route.query.page){
-          this.currentPage = parseInt(this.$route.query.page)
-      }
-  },
-  methods:{
-     onNextClicked(){
-         if (this.currentPage + 1 <= this.totalPages){
-            this.currentPage = this.currentPage + 1
-         }
-    },
-     onPreviousClicked(){
-       if (this.currentPage - 1 >= 1){
-            this.currentPage = this.currentPage - 1
-        }
+  mounted() {
+    if (this.$route.query.page) {
+      this.currentPage = parseInt(this.$route.query.page);
     }
-  }
-}
+  },
+  methods: {
+    onNextClicked() {
+      if (this.currentPage + 1 <= this.totalPages) {
+        this.currentPage = this.currentPage + 1;
+      }
+    },
+    onPreviousClicked() {
+      if (this.currentPage - 1 >= 1) {
+        this.currentPage = this.currentPage - 1;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-.container{
-   display: flex;
-   flex-direction: row;
-   justify-content: end;
-   flex-wrap: nowrap;
-   overflow-x: auto;
-   margin-top: 32px;
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  margin-top: 32px;
 }
 
-.buttons{
-    min-width: 40px;
-    height: 40px;
-    text-align: center;
-    line-height: 40px;
-    cursor: pointer;
-    border: 1px solid darkgray;
-    border-radius: 2px;
-    background: #f1f1f1;
-      -webkit-touch-callout: none; /* iOS Safari */
-    -webkit-user-select: none; /* Safari */
-     -khtml-user-select: none; /* Konqueror HTML */
-       -moz-user-select: none; /* Old versions of Firefox */
-        -ms-user-select: none; /* Internet Explorer/Edge */
-            user-select: none; /* Non-prefixed version, currently
-                                  supported by Chrome, Edge, Opera and Firefox */
+.buttons {
+  min-width: 40px;
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  cursor: pointer;
+  border: 1px solid darkgray;
+  border-radius: 2px;
+  background: #f1f1f1;
+  -webkit-touch-callout: none; 
+  -webkit-user-select: none;
+  -khtml-user-select: none; 
+  -moz-user-select: none; 
+  -ms-user-select: none; 
+  user-select: none;       
 }
 
-.result{
-    height: 50px;
-    line-height: 40px;
-    margin-left: 10px;
-    text-align: center;
+.result {
+  height: 50px;
+  line-height: 40px;
+  margin-left: 10px;
+  text-align: center;
 }
 
-.container input{
-    width: 60px;
-    min-width: 60px;
-    height: 38px;
-    outline: none;
-    margin-left: 4px;
-    margin-right: 10px;
-    direction: rtl;
-    border: 1px solid darkgray;
-    border-radius: 2px;
+.container input {
+  width: 60px;
+  min-width: 60px;
+  height: 38px;
+  outline: none;
+  margin-left: 4px;
+  margin-right: 10px;
+  direction: rtl;
+  border: 1px solid darkgray;
+  border-radius: 2px;
 }
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
@@ -105,8 +111,8 @@ input::-webkit-inner-spin-button {
 }
 
 /* Firefox */
-input[type=number] {
+input[type="number"] {
   -moz-appearance: textfield;
-  text-align: left; 
+  text-align: left;
 }
 </style>
